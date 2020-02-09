91e0ce3d510f9fa931d8741c864bf2bb5f716833
Renata Marques <Renata>
2020-02-09 16:05:00 -0300

    group model created

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
-  # has_many :teams, class_name: "Team"
+  belongs_to :tournament, class_name: "Tournament"
+  has_many :teams, class_name: "Team"
   # has_many :group_rankings, class_name: "GroupRanking"
 end
