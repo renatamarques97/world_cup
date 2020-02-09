5a41f63c5a57e269d69752e53be82d35c32e606b
Renata Marques <Renata>
2020-02-09 16:01:36 -0300

    creating endpoint for team creation

--- app/services/team_creator.rb
+++ app/services/team_creator.rb
@@ -1,12 +1,44 @@
-class TeamCreator
-  attr_reader :team_params
+# frozen_string_literal: true
 
-  def initialize(team_params:)
-    @team_params = team_params
+class TeamCreator
+  attr_reader :teams_params
+
+  def initialize(teams_params:)
+    @teams_params = teams_params
   end
 
   def created?
-    team = Team.new(team_params[:team])
-    team.save
+    return false if teams_valid?
+
+    teams_params.each_value do |team_name|
+      group = incomplete_group
+      team = Team.new(name: team_name, group_id: group.id)
+      team.save
+    end
+
+    if teams_valid?
+      match_creator = MatchCreator.new
+      match_creator.create_matches_phase_one
+      return true
+    end
+
+    Team.destroy_all
+    false
+  end
+
+  private
+
+  def teams_valid?
+    Team.count == 12
+  end
+
+  def incomplete_group
+    group = Group.all.sample
+
+    while group.teams.count > 3
+      group = Group.all.sample
+    end
+
+    group
   end
 end
