01501c94c4f85b61281d9373e9b8da48000bb19e
Renata Marques <Renata>
2020-02-09 23:41:55 -0300

    seed for tournament

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-tournament = Tournament.create({ name: 'World Cup' })
+tournament = Tournament.create(name: 'World Cup')
 
 Group.create([{ name: 'A', tournament_id: tournament.id },
               { name: 'B', tournament_id: tournament.id },
               { name: 'C', tournament_id: tournament.id },
               { name: 'D', tournament_id: tournament.id }])
 
-Phase.create([{ name: 'All Play All', tournament_id: tournament.id },
-              { name: 'Quarter Final', tournament_id: tournament.id },
-              { name: 'Semi-Final', tournament_id: tournament.id },
-              { name: 'Final', tournament_id: tournament.id }])
+Phase.create([{ name: 'first', tournament_id: tournament.id },
+              { name: 'second', tournament_id: tournament.id },
+              { name: 'semifinal', tournament_id: tournament.id },
+              { name: 'final', tournament_id: tournament.id }])
