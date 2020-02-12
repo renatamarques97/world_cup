1618aea9f92222920d34225f7ddac605a4f6cbd8
Renata Marques <renata.marques@codeminer42.com>
2020-02-12 13:43:02 -0300

    shoulda configuration

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should belong_to(:tournament) }
+  end
+
+  context "Valid Group" do
+    let(:valid_teams) {{
+      teams: [
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country },
+        { name: FFaker::Address.country }
+      ]
+    }}
+
+    before do
+      creator = TournamentCreator.new(params: valid_teams)
+      creator.perform
+    end
+
+    it 'It is a valid group' do
+      tournament = Tournament.last
+      group = Group.new(tournament_id: tournament.id, name: FFaker::Name)
+
+      expect(group.valid?).to be_truthy
+    end
+
+  end
 end
