28cfadab8e807165ea93897ccac65cba1eabf6d2
Renata Marques <renata.marques@codeminer42.com>
2020-02-12 15:09:06 -0300

    state machine tested

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'tournament' do
+    context '#aasm' do
+      it 'when state is none' do
+        expect(subject.state).to eq("none")
+      end
+
+      it 'when state is first' do
+        subject.next_phase
+        expect(subject.state).to eq("first")
+      end
+
+      it 'when state is second' do
+        subject.next_phase
+        subject.next_phase
+        expect(subject.state).to eq("second")
+      end
+
+      it 'when state is semifinal' do
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        expect(subject.state).to eq("semifinal")
+      end
+
+      it 'when state is final' do
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        expect(subject.state).to eq("final")
+      end
+
+      it 'when state is done' do
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        subject.next_phase
+        expect(subject.state).to eq("done")
+      end
+    end
+  end
 end
